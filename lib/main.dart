9473b036cb94122f5db4168562b0205de20c8674
Umar Fadil <umarfadil.skom@gmail.com>
2020-04-17 08:15:42 +0700

    First Commit + simple example

--- lib/main.dart
+++ lib/main.dart
@@ -9,18 +9,48 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Istagram",
-      theme: ThemeData(
-        primarySwatch: Colors.blue
-      ),
+      theme: ThemeData(primarySwatch: Colors.blue),
       home: Scaffold(
         body: Center(
-          child: Text(MunculkanKalimat().kalimatPanjang)),
+            child: Text(MunculkanKalimat(
+                    kalimatPanjang:
+                        "Selamat pagi, ini adalah kalimat panjang menggunakan Flutter.",
+                    jadiBesar: "Tidak")
+                .harusJadiBesarAtauTidak())),
       ),
     );
   }
 }
 
 class MunculkanKalimat {
-  String kalimatPanjang = "Selamat pagi, ini adalah kalimat panjang dengan Flutter";
-  String kalimatPendek = "Hello Worlds";
+  String kalimatPanjang, kalimatPendek, jadiBesar;
+
+  // ini namanya constructor atau pembentuk
+  MunculkanKalimat({this.kalimatPanjang, this.kalimatPendek, this.jadiBesar});
+
+  //MethodKalimatPanjang
+  String getKalimatPanjang() => kalimatPanjang;
+  setKalimatPanjang(String nilai) => kalimatPanjang = nilai;
+
+  //MethodKalimatPendek
+  String getKalimatPendek() => kalimatPendek;
+  setKalimatPendek(String nilai) => kalimatPendek = nilai;
+
+  //MethodSetUppercase
+  String getJadiBesar() => jadiBesar;
+  setJadiBesar(String nilai) => jadiBesar = nilai;
+
+  String jadiHurufBesarSemua() {
+    return kalimatPanjang.toUpperCase();
+  }
+
+  String harusJadiBesarAtauTidak() {
+    // catatan : jika apakah mengandung adalah iya maka kalimat panjang
+    // akan dirubah ke huruf besar
+    if (jadiBesar.contains("Ya")) {
+      return kalimatPanjang.toUpperCase();
+    } else {
+      return kalimatPanjang.toLowerCase();
+    }
+  }
 }
